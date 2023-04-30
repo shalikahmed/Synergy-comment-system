a4792d687fcdbc71866d9b02c07e29de08fafa41
shalikahmed <122047338+shalikahmed@users.noreply.github.com>
2023-05-01 03:14:51 +0530

    17th commit

---
+++ realtime-comment-system-master/db.js
@@ -0,0 +1,20 @@
+function dbConnect() {
+    // Db connection
+const mongoose = require('mongoose')
+const url = 'mongodb://127.0.0.1:27017/comments'
+
+mongoose.connect(url, {
+    userNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true
+})
+
+const connection = mongoose.connection
+connection.once('open', function() {
+    console.log('Database connected...')
+}).catch(function(err){
+    console.log('Connection failed...')
+})
+}
+
+module.exports = dbConnect
